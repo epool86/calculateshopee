d70505b9c9f0eb1f5580a92c9bafc6ecaf743152
epool86 <epool862@gmail.com>
2021-10-06 02:33:07 +0800

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -1,29 +1,26 @@
 var total = 0;
 var order = 0;
 function calculate(next){
-	if(next >= 0){
-		var opts = {
-			method: 'GET',      
-			headers: {}
-		};
-		fetch('https://shopee.com.my/api/v4/order/get_order_list?limit=5&list_type=3&offset='+next, opts).then(function (response) {
-			return response.json();
-		})
-		.then(function (body) {
-			var next_offset = body.data.next_offset;
-			if(next_offset >= 0){
-				for (let [key, value] of Object.entries(body.data.details_list)) {
-					var total_temp = value.info_card.final_total / 100000;
-					total += total_temp;
-					order++;
-		    		console.log("Order no " + order + ": ", "RM " + total_temp);
-				}
+	var opts = {
+		method: 'GET',      
+		headers: {}
+	};
+	fetch('https://shopee.com.my/api/v4/order/get_order_list?limit=5&list_type=3&offset='+next, opts).then(function (response) {
+		return response.json();
+	})
+	.then(function (body) {
+		var next_offset = body.data.next_offset;
+		if(next_offset >= 0){
+			for (let [key, value] of Object.entries(body.data.details_list)) {
+				var total_temp = value.info_card.final_total / 100000;
+				total += total_temp;
+				order++;
+	    		console.log("Order no " + order + ": ", "RM " + total_temp);
 			}
 			calculate(next_offset);
-		});
-	} else {
-		console.log('Calculation completed!');
-		console.log('GRAND TOTAL: RM ' + total);
-	}
-
+		} else {
+			console.log('Calculation completed!');
+			console.log('GRAND TOTAL: RM ' + total);
+		}
+	});
 }
